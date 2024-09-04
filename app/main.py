1503943eeae3ec357ab3c3e161805ad94dcea1c9
statelinecat <statelinecat@gmail.com>
2024-09-04 14:52:52 +0300

    add fon

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import requests
+from googletrans import Translator
 
 app = Flask(__name__)
+translator = Translator()
 
 @app.route('/')
 def index():
@@ -9,7 +11,9 @@ def index():
     data = response.json()
     quote = data.get('content', 'No quote available')
     author = data.get('author', 'Unknown')
-    return render_template('index.html', quote=quote, author=author)
+    quote_ru = translator.translate(quote, dest='ru').text
+    author_ru = translator.translate(author, dest='ru').text
+    return render_template('index.html', quote=quote_ru, author=author_ru)
 
 if __name__ == '__main__':
     app.run(debug=True)
